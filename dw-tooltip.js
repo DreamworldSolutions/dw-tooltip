866fadc0e03c0a4d24189e45a2fceda0f4d9039d
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-02-05 15:23:35 +0530

    fix: fix offset issue with latest version of tippy.js

--- dw-tooltip.js
+++ dw-tooltip.js
@@ -67,9 +67,9 @@ class DWTooltip extends LitElement {
       /**
        * Input property
        * How far in pixels the tippy element is from the reference element.
-       * Default value - 0
+       * Default value - [0, 0]
        */
-      offset: { type: Number },
+      offset: { type: Array },
 
       /*
        * Themes added as classes (each separated by a space) to the tippy element's  classList
@@ -99,7 +99,7 @@ class DWTooltip extends LitElement {
     this.trigger = 'mouseenter';
     this.placement = 'top';
     this.animation = 'shift-away';
-    this.offset = 0;
+    this.offset = [0, 0];
     this.theme = 'material';
     this.disabled = false;
   }
