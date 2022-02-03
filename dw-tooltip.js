269fb9c2eabab6a402ba53454896344b39410ef4
devangdreamworld <devangk@codetailor.in>
2022-02-03 11:53:42 +0530

    fix: Provides a CSS Mixin to configure material theme for tippy.js

--- dw-tooltip.js
+++ dw-tooltip.js
@@ -11,8 +11,17 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 import { html, css } from 'lit-element';
 import { LitElement } from '@dreamworld/pwa-helpers/lit-element.js';
 import tippy, {sticky} from 'tippy.js'
+import { caption } from '@dreamworld/material-styles/typography-literals.js';
 
 let appendToElement;
+
+export const DWTooltipStyle = css`
+  background-color: var(--dw-tooltip-background-color);
+  color: var(--mdc-theme-surface);
+  border-radius: var(--dw-tooltip-border-radius, 4px);
+  padding: var(--dw-tooltip-padding, 8px);
+  ${caption};
+`;
 
 export class DWTooltip extends LitElement {
   static get styles() {
